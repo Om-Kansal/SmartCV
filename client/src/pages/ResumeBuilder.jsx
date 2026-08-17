bc34b023230e91417888605f5cd7d58dcce1e027
Om Kansal <omkansal285@gmail.com>
2026-08-17 13:38:09 +0530

    added save changes, share, public/private and download buttons functionalities on resume builder page

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,4 +1,4 @@
-import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
+import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, DownloadIcon, EyeIcon, EyeOff, FileText, FolderIcon, GraduationCap, Share2, Share2Icon, Sparkles, User } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import {Link, useParams } from 'react-router-dom'
 import PersonalInfoForm from '../components/PersonalInfoForm'
@@ -56,10 +56,29 @@ const ResumeBuilder = () => {
     loadExistingResume()
   },[])
 
+  const changeResumeVisibility = async () => {
+    setResumeData({...resumeData, public: !resumeData.public})
+  }
+
+  const handleShare = () => {
+    const frontendUrl = window.location.href.split('/app/')[0]
+    const resumeUrl = frontendUrl + '/view/' + resumeID;
+
+    if(navigator.share){
+      navigator.share({url: resumeUrl, text: "My Resume", })
+    }else{
+      alert('Share not supported on this browser.')
+    }
+  }
+
+  const downloadResume = () => {
+    window.print()
+  }
+
   return (
     <div>
-      <div>
-        <Link to={'/app'} className='inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all' >
+      <div >
+        <Link to={'/app'} className='ml-20 inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all' >
           <ArrowLeftIcon className='size-4'/>Back to Dashboard
         </Link>
       </div>
@@ -67,7 +86,7 @@ const ResumeBuilder = () => {
       <div className='max-w-7xl mx-auto px-4 pb-8'>
         <div className='grid lg:grid-cols-12 gap-8'>
           {/* left panel - form */}
-          <div className='relative lg:col-span-5 rounded-lg overflow-hidden'>
+          <div className='mt-11 relative lg:col-span-5 rounded-lg overflow-hidden'>
             <div className='bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1'>
               {/* progress bar using activeSectionIndex */}
               <hr className='absolute top-0 left-0 right-0 border-2 border-gray-200' />
@@ -120,14 +139,28 @@ const ResumeBuilder = () => {
                   
               </div>
 
+              <button className='bg-gradient-to-br from-green-100 to-green-200 ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm'>
+                Save Changes
+              </button>
+
             </div>
           </div>
 
           {/* right panel - preview */}
           <div className='lg:col-span-7 max-lg:mt-6'>
-              <div>
-                {/* ----- buttons ---- */}
-
+              <div className=' mb-3 flex items-center justify-end gap-2'>
+                {resumeData.public && (
+                  <button onClick={handleShare} className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors'>
+                    <Share2Icon className='size-4'/>Share
+                  </button>
+                )}
+                <button onClick={changeResumeVisibility} className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 rounded-lg ring-purple-300 rounded-1g hover:ring transition-colors'>
+                  {resumeData.public ? <EyeIcon className='size-4'/> : <EyeOff className='size-4'/>}
+                  {resumeData.public ? 'public' : 'private'}
+                </button>
+                <button onClick={downloadResume} className='flex items-center gap-2 px-6 py-2 text-xs bg-gradient-to-br from-green-100 to-green-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors'>
+                  <DownloadIcon className='size-4'/> Download
+                </button>
               </div>
 
               {/* ---- resume preview */}
