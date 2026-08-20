e4bbf4dbc282980fc9777b5d081f010218a2aa0c
Om Kansal <omkansal285@gmail.com>
2026-08-20 20:24:09 +0530

    created getResumeById and getPublicResumeById controllers

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -34,3 +34,45 @@ export const deleteResume = async (req, res) => {
         return res.status(400).json({message: error.message})
     }
 }
+
+// get user resume by id
+// GET: /api/resumes/get
+export const getResumeById = async (req, res) => {
+    try {
+       const userId = req.userId;
+       const {resumeId} = req.params;
+
+       const resume = await Resume.findOne({userId, _id: resumeId}) 
+
+       if(!resume){
+        return res.status(404).json({message: "resume not found"})
+       } 
+
+       //return success message
+       resume.__v = undefined;
+       resume.createdAt = undefined;
+       resume.updatedAt = undefined;
+
+       return res.status(200).json({resume}) 
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
+
+// get resume by id public
+// GET: /api/resumes/public
+export const getPublicResumeById = async (req, res) => {
+    try {
+        const { resumeId } = req.params;
+        const resume = await Resume.findOne({public: true, _id: resumeId})
+
+        if(!resume){
+        return res.status(404).json({message: "resume not found"})
+        } 
+
+        return res.status(200).json({resume}) 
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+
+    }
+}
